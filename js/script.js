54e9bfe3218f036c28db3f530485bbf1f4592775
Brian Anderson <banderson@dandb.com>
2013-07-06 21:28:21 -0700

    add in css pre-loader

--- js/script.js
+++ js/script.js
@@ -205,6 +205,7 @@ function setPopupLoaders(){
 	}
 }
 function setPageFunctions(page){
+	$('#floatingCirclesG').hide();
 	$('#page1,#page2,#page3,#page4,#page5,#page6').removeAttr('style');
 	$('#page'+page).css('backgroundPosition','0 0');
 

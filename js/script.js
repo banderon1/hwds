e6afaf31aecb9e6398f9cdb2464673374a5c9545
Brian Anderson <banderson@dandb.com>
2013-07-11 21:56:53 -0700

    update images on the homepage carousel

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ function IndexCtrl($scope){
 		auto:					true,
 		interval:				5000,
 		continuous:				false,
-		loading:				true,
+		loading:				false,
 		tooltip_width:			200,
 		tooltip_icon_width:		32,
 		tooltip_icon_height:	32,
@@ -128,7 +128,7 @@ function IndexCtrl($scope){
 		thumbnails_direction:	'vertical', /* vertical/horizontal */
 		thumbnails_slidex:		1, /* 0 = auto / 1 = slide one thumbnail / 2 = slide two thumbnails / etc. */
 		dynamic_height:			false, /* For dynamic height to work in webkit you need to set the width and height of images in the source. Usually works to only set the dimension of the first slide in the showcase. */
-		speed_change:			true, /* Set to true to prevent users from swithing more then one slide at once. */
+		speed_change:			true, /* Set to true to prevent users from switching more then one slide at once. */
 		viewline:				false, /* If set to true content_width, thumbnails, transition and dynamic_height will be disabled. As for dynamic height you need to set the width and height of images in the source. */
 		custom_function:		null /* Define a custom function that runs on content change */
 	});
